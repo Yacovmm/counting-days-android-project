4df856ac6e1983d54a07a305f44509acd17be068
Yacov <yayaro94@gmail.com>
2021-01-04 21:52:30 -0300

    Added date msg created to the cell Item

--- app/src/main/java/com/yacov/countingdays/ui/fragments/MessageCell.kt
+++ app/src/main/java/com/yacov/countingdays/ui/fragments/MessageCell.kt
@@ -5,6 +5,8 @@ import android.widget.TextView
 import com.yacov.countingdays.R
 import com.yacov.countingdays.data.entities.MessageEntity
 import com.yacov.countingdays.ui.adapters.BaseCell
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MessageCell(view: View) : BaseCell(view) {
 
@@ -14,10 +16,28 @@ class MessageCell(view: View) : BaseCell(view) {
 
     private var titleText: TextView = view.findViewById(R.id.title_tv)
     private var msgText: TextView = view.findViewById(R.id.msg_tv)
+    private var dateTxt: TextView = view.findViewById(R.id.date_tv)
 
     fun setModel(messageEntity: MessageEntity) {
 
         titleText.text = messageEntity.title
-        msgText.text = "${messageEntity.msg.slice(0..70)}..."
+        if (messageEntity.msg.length > 70)
+            msgText.text = "${messageEntity.msg.slice(0..70)}..."
+        else
+            msgText.text = messageEntity.msg
+
+        dateTxt.text = "Enviada em: ${formatDate(messageEntity.createdAt, outFormat = "dd/MM/dd HH:mm")}"
+    }
+
+    fun formatDate(date: Date, outFormat: String = "yyyy-MM-dd"): String {
+        val simpleDateFormat = SimpleDateFormat(outFormat, Locale("pt", "BR"))
+        return simpleDateFormat.format(date)
+    }
+
+    fun formatDate(dateString: String, dateStringFormat: String = "yyyy-MM-dd'T'HH:mm:ss.SSS", outFormat: String = "yyyy-MM-dd"): String {
+        val sdf = SimpleDateFormat(dateStringFormat, Locale("pt", "BR"))
+        val date = sdf.parse(dateString) ?: Date()
+
+        return formatDate(date, outFormat)
     }
 }
